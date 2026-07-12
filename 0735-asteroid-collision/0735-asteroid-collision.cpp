1a497651c7cded4957870eccb5a70c7a571f9f53
atchyutbhallamudi <atchyutbhallamudi@gmail.com>
2026-07-12 12:52:37 +0530

    Time: 0 ms (100%), Space: 21.9 MB (69.77%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> st;
-        for(auto v : asteroids){
-            if(v>0 || st.empty())st.push(v);
-            else{
-                st.push(v);
-                while(st.size() > 1 && st.top() < 0){
-                    int top = st.top();
-                    st.pop();
-                    int topb = st.top();
-                    if(topb < 0){
-                        st.push(top);
-                        break;
-                    }
-                    st.pop();
-                    if(abs(top) == abs(topb)) continue;
-                    else if(abs(top) > abs(topb)) st.push(top);
-                    else st.push(topb);
-                }
-            }
-        }
+    vector<int> asteroidCollision(vector<int>& a) {
+        int n = a.size();
         vector<int> ans;
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();            
+        stack<int> st;
+        
+        for(int i = 0; i < n; i++) {
+            if(a[i] > 0) {
+                st.push(a[i]);
+            } else {
+                while(!st.empty() && st.top() < -a[i]) {
+                    st.pop();
+                }
+                if(st.empty()) {
+                    ans.push_back(a[i]);
+                } else if(st.top() == -a[i]) {
+                    st.pop();
+                }
+            }         
         }
-        reverse(ans.begin(),ans.end());
+        
+        vector<int> t;
+        while(!st.empty()) {
+            t.push_back(st.top());
+            st.pop();
+        }
+        
+        for(int i = t.size() - 1; i >= 0; i--) {
+            ans.push_back(t[i]);
+        }
+
         return ans;
     }
 };
