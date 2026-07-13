a9941b98345ecaffced9c546503a334344c7569a
atchyutbhallamudi <atchyutbhallamudi@gmail.com>
2026-07-14 03:06:33 +0530

    Time: 23 ms (57.99%), Space: 141.5 MB (21.67%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,32 +1,50 @@
 class Solution {
 public:
+    #define F first
+    #define S second
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> ans;
-        priority_queue<pair<int,int>> pq;
+        deque<pair<int,int>> dq;
 
         if(k == 1) return nums;
-
-        for(int i = 0; i < k; i++){
-            pq.push({nums[i], i});
+        
+        // 1. Process the first window (up to k-1)
+        dq.push_back({nums[0],0});
+        for(int i = 1; i < k; i++){
+            // Added !dq.empty() to prevent runtime errors
+            while(!dq.empty() && nums[i] > dq.back().F){
+                dq.pop_back();
+            }
+            // Changed push_front to push_back
+            dq.push_back({nums[i],i}); 
         }
-
-        ans.push_back(pq.top().first);
-
+        
+        // Max is now safely at the front
+        ans.push_back(dq.front().F); 
+        
+        // 2. Process the rest of the array with left/right pointers
         int left = 0, right = k - 1;
         while(right < n - 1){
             left++;
             right++;
 
-            pq.push({nums[right], right});
+            // Pop smaller elements from the back (added empty check)
+            while(!dq.empty() && nums[right] > dq.back().F){
+                dq.pop_back();
+            }
+            // Push new element to the back
+            dq.push_back({nums[right],right});
 
-            while(!pq.empty() && pq.top().second < left) pq.pop();
+            // Remove out-of-bounds elements from the FRONT
+            while(!dq.empty() && dq.front().S < left) {
+                dq.pop_front(); 
+            }
 
-            ans.push_back(pq.top().first);
-            
+            // Max is at the front
+            ans.push_back(dq.front().F);
         }
 
         return ans;
-
     }
 };
