ec481af5947610c4da7b8705d3f6663b61a72599
atchyutbhallamudi <atchyutbhallamudi@gmail.com>
2026-07-26 13:40:49 +0530

    Time: 0 ms (100%), Space: 9.1 MB (57.85%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dp[35][35];
+
+    int rec(int i, int j, string& s, string& p) {
+        // Base case: If we reached the end of pattern
+        if (j == p.size()) return i == s.size();
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        // Check if current characters match
+        bool first_match = (i < s.size() && (s[i] == p[j] || p[j] == '.'));
+
+        bool ans = false;
+
+        // Lookahead: Check if the next character in pattern is '*'
+        if (j + 1 < p.size() && p[j + 1] == '*') {
+            // Choice 1: Ignore 'char*' (use 0 times) -> move j past '*'
+            // Choice 2: Use 'char*' (use 1+ times) -> advance i if first_match is true
+            ans = rec(i, j + 2, s, p) || (first_match && rec(i + 1, j, s, p));
+        } else {
+            // Standard single character match
+            ans = first_match && rec(i + 1, j + 1, s, p);
+        }
+
+        return dp[i][j] = ans;
+    }
+
+    bool isMatch(string s, string p) {
+        memset(dp, -1, sizeof(dp));
+        return rec(0, 0, s, p);
+    }
+};
