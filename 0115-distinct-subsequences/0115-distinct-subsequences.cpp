649a281f7535d98b839051386d0a9de8a47ee0b8
atchyutbhallamudi <atchyutbhallamudi@gmail.com>
2026-09-08 01:05:11 +0530

    Time: 42 ms (11.43%), Space: 27.1 MB (43.27%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int dp[1010][1010];
-    int rec(int i, int j, string& s, string& t){
-        //pr
-        //bc
-        if(j == t.size()) return 1;
-        if(i == s.size()) return 0;
-        //cc
-        if(dp[i][j] != -1) return dp[i][j];
-        //transition
+    int rec(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+        // pr
+        // bc
+        if (j == t.size())
+            return 1;
+        if (i == s.size())
+            return 0;
+        // cc
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        // transition
         int ans = 0;
-        if(s[i] == t[j]){
-            ans+=rec(i+1,j,s,t) + rec(i+1,j+1,s,t);
-        }
-        else{
-            ans+= rec(i+1,j,s,t);
+        if (s[i] == t[j]) {
+            ans += rec(i + 1, j, s, t,dp) + rec(i + 1, j + 1, s, t,dp);
+        } else {
+            ans += rec(i + 1, j, s, t,dp);
         }
         return dp[i][j] = ans;
     }
     int numDistinct(string s, string t) {
-        memset(dp,-1,sizeof(dp));
-        return rec(0,0,s,t);  
+        vector<vector<int>>dp(s.size()+1,vector<int>(t.size()+1,-1));
+        return rec(0, 0, s, t,dp);
     }
 };
